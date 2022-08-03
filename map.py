1339e8731dcc3a039d4681f76bb045d65c2c275b
Nexailter <100661158+Nexailter@users.noreply.github.com>
2022-08-02 20:34:47 -0700

    starting on more map stuff

--- map.py
+++ map.py
@@ -1,20 +1,39 @@
-from asyncio.proactor_events import _ProactorDuplexPipeTransport
-from winsound import PlaySound
 
 
-grid = [[,,,,,,,,,,],
-        [,,,,,,,,,,],
-        [,,,,,,,,,,],
-        [,,,,,,,,,,],
-        [,,,,,,,,,,],
-        [,,,,,,,,,,],
-        [,,,,,,,,,,],
-        [,,,,,,,,,,],
-        [,,,,,,,,,,],
-        [,,,,,,,,,,]]
+# grid = [[,,,,,,,,,,],
+#         [,,,,,,,,,,],
+#         [,,,,,,,,,,],
+#         [,,,,,,,,,,],
+#         [,,,,,,,,,,],
+#         [,,,,,,,,,,],
+#         [,,,,,,,,,,],
+#         [,,,,,,,,,,],
+#         [,,,,,,,,,,],
+#         [,,,,,,,,,,]]
 
 
 #desert,plains,ocean,mountains,restricted wilderness,snowy,deserted village,populated village
 
 
-grid = [[,,,,,,,,,,], [,,,,,,,,,,], [,,,,,,,,,,], [,,,,,,,,,,], [,,,,,,,,,,], [,,,,,,,,,,,], [,,,,,,,,,,], [,,,,,,,,,,], [,,,,,,,,,,], [,,,,,,,,,,]]
+map = [
+        ['1a','1b'],
+        ['2a','2b']
+        ]
+
+#print (map)
+
+# in this coordinate system y axis is first x is last
+pos = [0,0]
+
+def nav():
+        while True:
+                row = pos[0]
+                col = pos[1] #column
+                print('you are currently at zone ' + map[row][col])
+                direct = input('choose direction \na = east\nb = west\nc = north\nd = south\n')
+                if direct == 'a':
+                        pos[1] = col + 1
+
+                
+
+nav()
