086633f7b1a62320e3a84ee5917669809bb31af8
Nexailter <100661158+Nexailter@users.noreply.github.com>
2022-08-23 20:05:52 -0700

    asdf

--- map.py
+++ map.py
@@ -21,7 +21,10 @@ map = [
         ]
 
 #print (map)
-
+             
+            
+       
+#Homework: Use len() of map to create boundaries that change according to the map size.
 # in this coordinate system y axis is first x is last
 pos = [0,0]
 
@@ -31,9 +34,15 @@ def nav():
                 col = pos[1] #column
                 print('you are currently at zone ' + map[row][col])
                 direct = input('choose direction \na = east\nb = west\nc = north\nd = south\n')
-                if direct == 'a':
+                if direct == 'a' and col <1:
                         pos[1] = col + 1
-
-                
+                elif  direct == 'b' and col >0:
+                        pos[1] = col - 1
+                elif direct == 'c' and row >0:
+                        pos[0] = row - 1
+                elif direct == 'd' and row <1:
+                        pos[0] = row + 1
+                else:
+                        print('Please Choose a Valdid Direction')
 
 nav()
